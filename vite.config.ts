250a03c4555ce991de51148841df70f2f48b47a4
maraaa1m <mh18marambdz@gmail.com>
2025-12-28 14:59:11 +0100

    Fix: Setting base path in vite.config.ts for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        base: '/vite-test-deploy/',
       },
       plugins: [react()],
       define: {
